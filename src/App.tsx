1f6b2ab17d615ed81d1ea10ac9e3d20f33334011
BlueSetbyeol <setbyeol.c@gmail.com>
2024-10-22 11:54:07 +0200

    correction extension fichier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
-import pokemon from './assets/pokemon.jpg';
-import HelloPokemon from './components/hello-pokemon';
+import HelloPokemon from './components/hello-pokemon.tsx';
 import './App.css'
 
 function App() {
